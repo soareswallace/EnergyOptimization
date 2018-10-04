c9a4bcfcbe77f2acf3ed53fd1711071e895201f4
Wallace Soares <soareswallace@icloud.com>
2018-10-04 11:14:17 -0300

    index page working properly

--- Energy/ehouse/apps.py
+++ Energy/ehouse/apps.py
@@ -4,5 +4,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class EnergyConsumptionConfig(AppConfig):
-    name = 'Energy_Consumption'
+class EhouseConfig(AppConfig):
+    name = 'ehouse'
